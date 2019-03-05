c8b53d19a9c6ec94dd485f8fba619663730309de
William Bogans <william.bogans@students.makeschool.com>
2019-03-04 20:17:09 -0800

    DB connection working

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const mongoose = require('mongoose');
-const errorHandler = require('errorhandler');
 
 //Configure mongoose's promise to global promise
 mongoose.promise = global.Promise;
 
-//Configure isProduction variable
-const isProduction = process.env.NODE_ENV === 'production';
 
+const PORT = process.env.PORT || 3000;
 //Initiate our app
 const app = express();
 
@@ -20,10 +18,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
-if (!isProduction) {
-  app.use(errorHandler());
-}
-
 //Configure Mongoose
 mongoose.connect('mongodb://test:testpassword1@ds147073.mlab.com:47073/bew');
 mongoose.set('debug', true);
@@ -32,33 +26,13 @@ require('./models/Users');
 require('./config/passport');
 app.use(require('./controllers'));
 
-//Error handlers & middlewares
-if (!isProduction) {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-
-    res.json({
-      errors: {
-        message: err.message,
-        error: err,
-      },
-    });
-  });
-}
-
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-
-  res.json({
-    errors: {
-      message: err.message,
-      error: {},
-    },
-  });
-});
 
 app.get('/', (req, res) => {
   res.send('You connected to me')
 })
 
-app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
+app.get('/users/api', (req, res) => {
+  res.json({ "message": "hey" })
+})
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
